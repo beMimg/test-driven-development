372f8abd5b92d8491475bd317f1b55e1a9a796ff
bernyig <bernardommguerreiro@gmail.com>
2023-10-27 00:36:40 +0200

    calculator multiply functional

--- calculator.js
+++ calculator.js
@@ -13,10 +13,16 @@ const divide = function (a, b) {
   return a / b;
 };
 
+const multiply = function (a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') return null;
+  return a * b;
+};
+
 const calculator = {
   add,
   sub,
   divide,
+  multiply,
 };
 
 module.exports = calculator;
