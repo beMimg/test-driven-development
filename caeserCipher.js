9281118ed80c3cc39b3c97a105b3f6a7f12f0c55
bernyig <bernardommguerreiro@gmail.com>
2023-10-27 01:02:25 +0200

    caeserCipher functional

---
+++ caeserCipher.js
@@ -0,0 +1,19 @@
+function caeserCipher(word, factor) {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  let encryptedWord = '';
+  if (factor < 0) {
+    return null;
+  }
+  if (!factor) {
+    fator = 0;
+  }
+  for (let i = 0; i < word.length; i++) {
+    const letter = word[i];
+    if (alphabet.includes(letter)) {
+      let currentIndex = alphabet.indexOf(letter);
+      let newIndex = (currentIndex + factor) % 26;
+      encryptedWord += alphabet[newIndex];
+    }
+  }
+  return encryptedWord;
+}
