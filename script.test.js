b593c7e528ca48fdc66569b3d7d615aba778c2c0
bernyig <bernardommguerreiro@gmail.com>
2023-10-27 00:25:19 +0200

    calculator sub test written

--- script.test.js
+++ script.test.js
@@ -21,12 +21,23 @@ describe('Reverse String', () => {
 });
 
 describe('Calculator Add', () => {
-  it('should should return the sum of two numbers', () => {
+  it('should return the sum of two numbers', () => {
     expect(calculator.add(2, 2)).toBe(4);
   });
-  it('should handle not numbers', () => {
+  it('should handle NaN', () => {
     expect(calculator.add('a', 1)).toBeNull();
     expect(calculator.add('a', 'a')).toBeNull();
     expect(calculator.add(1, 'a')).toBeNull();
+  });
+});
+
+describe('Calculator Sub', () => {
+  it('should return the subtraction of two numbers', () => {
+    expect(calculator.sub(3, 2)).toBe(1);
+  });
+  it('should handle NaN', () => {
+    expect(calculator.sub('a', 1)).toBeNull();
+    expect(calculator.sub('a', 'a')).toBeNull();
+    expect(calculator.sub(1, 'a')).toBeNull();
   });
 });
