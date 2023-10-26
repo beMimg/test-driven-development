d21db2b5ab55c922faf782295e9e3644615f8f05
bernyig <bernardommguerreiro@gmail.com>
2023-10-27 00:33:23 +0200

    calculator divide functional and passed tests

--- calculator.js
+++ calculator.js
@@ -7,9 +7,16 @@ const sub = function (a, b) {
   if (typeof a !== 'number' || typeof b !== 'number') return null;
   return a - b;
 };
+
+const divide = function (a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') return null;
+  return a / b;
+};
+
 const calculator = {
   add,
   sub,
+  divide,
 };
 
 module.exports = calculator;
