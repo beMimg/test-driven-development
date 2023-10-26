996b32c99ffc285c65c15b2f74cbf52714280c05
bernyig <bernardommguerreiro@gmail.com>
2023-10-26 19:01:17 +0200

    capitalize functional and passed test

--- capitalize.js
+++ capitalize.js
@@ -1,4 +1,7 @@
 function capitalizeFirstLetter(word) {
+  if (typeof word === 'number') {
+    return null;
+  }
   const firstLetter = word[0].toUpperCase();
   const afterFirstLetter = word.slice(1, word.length);
   return firstLetter + afterFirstLetter;
