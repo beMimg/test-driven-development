ab55f51cc2182089f1f03fb080e85cb4bbc12dcb
bernyig <bernardommguerreiro@gmail.com>
2023-10-26 19:27:41 +0200

    calculator testing written

--- script.test.js
+++ script.test.js
@@ -1,6 +1,6 @@
 const capitalizeFirstLetter = require('./capitalize.js');
 const reverseString = require('./reverseString');
-
+const { calculator } = require('./calculator');
 describe('Capitalize', () => {
   it('should return first letter capitalized', () => {
     expect(capitalizeFirstLetter('abc')).toBe('Abc');
@@ -16,5 +16,16 @@ describe('Reverse String', () => {
   });
   it('should handle numbers', () => {
     expect(reverseString(1)).toBeNull();
+  });
+});
+
+describe('Calculator Add', () => {
+  it('should should return the sum of two numbers', () => {
+    expect(calculator.add(2, 2)).toBe(4);
+  });
+  it('should handle not numbers', () => {
+    expect(calculator.add(NaN, 1)).toBeNull();
+    expect(calculator.add(NaN, NaN)).toBeNull();
+    expect(calculator.add(1, NaN)).toBeNull();
   });
 });
